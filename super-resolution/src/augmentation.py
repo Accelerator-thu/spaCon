42d0db14e5add4d2b58ec89b679b4e02b70695ab
Yufan Cao <caoyf20@mails.tsinghua.edu.cn>
2023-11-04 01:57:33 +0800

    update

--- super-resolution/src/augmentation.py
+++ super-resolution/src/augmentation.py
@@ -2,63 +2,51 @@ import random
 
 import cv2
 import numpy as np
+import torchvision.transforms as transforms
 
 # Image Augmentation
 
-def randomCrop(img, crop_size):
-    """
-    Randomly crop the image to the given size.
-    """
-    h, w = img.shape[:2]
-    x, y = np.random.randint(0, h - crop_size[0]), np.random.randint(0, w - crop_size[1])
-    return img[x:x+crop_size[0], y:y+crop_size[1]]
+def randomCropWithCenterResized(img, size, center, crop_scale):
     
-def randomRotate(img):
-    """
-    Randomly rotate the image by the given angle.
-    """
-    h, w = img.shape[:2]
-    center = (w / 2, h / 2)
-    angle = np.random.randint(0, 360)
-    M = cv2.getRotationMatrix2D(center, angle, 1.0)
-    return cv2.warpAffine(img, M, (w, h))
+    center0, center1 = center
+    max0 = img.shape[0] - 2 * np.abs(img.shape[0] // 2 - center0) - 1
+    max1 = img.shape[1] - 2 * np.abs(img.shape[1] // 2 - center1) - 1
+    
+    img_tmp = img.copy()
+    img_tmp = img_tmp[center0 - max0 // 2:center0 + max0 // 2 + 1, center1 - max1 // 2:center1 + max1 // 2 + 1]
+    # random crop
+    crop0 = size[0] * crop_scale[0]
+    crop1 = size[1] * crop_scale[1]
+    img_tmp = transforms.CenterCrop(size=(crop0, crop1))(img_tmp)
+    
+    # resize
+    img_tmp = cv2.resize(img_tmp, size[::-1])
+    
+    return img_tmp
 
-def randomFlip(img):
-    """
-    Randomly flip the image.
-    """
-    flipCode = random.randint(-1, 1)
-    return cv2.flip(img, flipCode)
+def randomCropWithCenterInside(img, size, center, crop_scale):
+    
+    center0, center1 = center
+    crop0, crop1 = size[0] * crop_scale[0], size[1] * crop_scale[1]
+    dev0, dev1 = random.randint(-crop0 // 2, crop0 // 2), random.randint(-crop1 // 2, crop1 // 2)
+    center0, center1 = center0 + dev0, center1 + dev1
+    
+    img_tmp = img.copy()
+    img_tmp = img_tmp[max(center0 - crop0 // 2, 0):min(center0 + crop0 // 2 + 1, img.shape[0]), 
+                      max(center1 - crop1 // 2, 0):min(center1 + crop1 // 2 + 1, img.shape[1])]
+    
+    img_tmp = cv2.resize(img_tmp, size[::-1])
+    
+    return img_tmp
 
-def randomBlur(img, kernel_size):
-    """
-    Randomly blur the image.
-    """
-    return cv2.blur(img, (kernel_size, kernel_size))
-
-def colorDistortion(img):
-    """
-    Randomly distort the color of the image.
-    """
-    # Randomly shuffle the channels of the image
-    img = img[..., np.random.permutation(3)]
+def gaussianBlur(img, kernel_size, sigma):
+        
+    img_tmp = img.copy()
+    img_tmp = cv2.GaussianBlur(img_tmp, (kernel_size, kernel_size), sigma)
     
-    # Randomly scale the channels of the image
-    scale = np.random.uniform(0.8, 1.2, size=3)
-    img[..., 0] *= scale[0]
-    img[..., 1] *= scale[1]
-    img[..., 2] *= scale[2]
+    return img_tmp
     
-    # Randomly shift the channels of the image
-    shift = np.random.uniform(-0.2, 0.2, size=3)
-    img[..., 0] += shift[0]
-    img[..., 1] += shift[1]
-    img[..., 2] += shift[2]
-    
-    # Clip the image to the valid pixel range
-    img = np.clip(img, 0, 255)
-    
-    return img
+        
     
 # Graph Augmentation
 
